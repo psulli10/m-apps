9e547250db0c9aee75b86377a9b5dc1c57c97b42
Peter Sullivan <petersullivan1985@gmail.com>
2021-01-09 11:30:01 +0000

    state added to app for fetch to set to

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    console.log('use effect on render...')
+  }, [])
+
   return (
     <>
     <h1>M-Apps - getting ready to fetch</h1>
