5c7d25ed6894217dbe4d8e5c36a80614a07f9a03
Peter Sullivan <petersullivan1985@gmail.com>
2021-01-10 13:38:17 +0000

    result component added to quiz container

---
+++ client/src/components/ResultComponent.js
@@ -0,0 +1,11 @@
+const ResultComponent = ({ score }) => {
+
+  return(
+    <>
+    <h3>This is a result component</h3>
+    <h5>You scored {score} out of 5</h5>
+    </>
+  )
+};
+
+export default ResultComponent;
